0a3311d9c45493ab2aca9723e410fcc133aee73b
valenrosasc <v.rosas@utp.edu.co>
2025-06-04 14:01:45 -0500

    Cambios vr

--- usuarios/models.py
+++
@@ -1,7 +0,0 @@
-# usuarios/models.py
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class Usuario(AbstractUser):
-    # Puedes agregar campos adicionales aquí si los necesitas
-    pass
